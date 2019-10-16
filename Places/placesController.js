3c7cf20d2dddeb6048fc320d6db6dcbb61f1e038
Subhadeep Pal <subhadeep.pal@yulu.bike>
2019-10-16 11:00:11 +0530

    Added promise block

--- Places/placesController.js
+++ Places/placesController.js
@@ -3,65 +3,81 @@ var dbConnection = require('../DatabaseConnection/dbConnection');
 var fs = require('fs');
 
 function getAllPlaces(apiVersion, req, res, next) {
-    Place.find(function(err, response){
+    Place.find(function (err, response) {
         if (!err)
             res.status(200).send(response);
         else
-            res.status(400).send({"error":"Failed to save, check data type" + err});
-     });
-    
+            res.status(400).send({ "error": "Failed to save, check data type" + err });
+    });
+
 }
 
 function getPlaceWithId(apiVersion, req, res, next) {
-    Place.findById(req.params.id, function(err, response){
+    Place.findById(req.params.id, function (err, response) {
         if (!err)
             res.status(200).send(response);
         else
-            res.status(400).send({"error":"Failed to save, check data type" + err});
-     });
+            res.status(400).send({ "error": "Failed to save, check data type" + err });
+    });
 }
 
-function createPlace(apiVersion, req, res, next) {
+async function createPlace(apiVersion, req, res, next) {
     var placesInfo = req.body; //Get the parsed information
     var image = req.files;
-    if(image) {
-        if(image.image.mimetype != 'image/jpeg')
-            return res.status(400).send({"error":"File should be image jepg"});
-        if(image.image.size > 500000)
-            return res.status(400).send({"error":"File should be lesser than 500kb"});
-            image.image.mv('/Users/deep/Development/UploadedImages'+image.image.tempFilePath+'.jpg', function(err) {
-            if (err)
-              return res.status(500).send(err);
-        });
+    if (image) {
+        if (image.image.mimetype != 'image/jpeg')
+            return res.status(400).send({ "error": "File should be image jepg" });
+        if (image.image.size > 500000)
+            return res.status(400).send({ "error": "File should be lesser than 500kb" });
+
+        try {
+            await moveFile(image);
+        } catch(e) {
+            return res.status(500).send(e);
+        }
     }
-    if(!placesInfo.title || !placesInfo.latitude || !placesInfo.longitude){
-        res.status(400).send({"error":"Params Missing"});
-     } else {
+    if (!placesInfo.title || !placesInfo.latitude || !placesInfo.longitude) {
+        res.status(400).send({ "error": "Params Missing" });
+    } else {
         var place = new Place({
-           title: placesInfo.title,
-           latitude: placesInfo.latitude,
-           longitude: placesInfo.longitude,
-           description: placesInfo.description
+            title: placesInfo.title,
+            latitude: placesInfo.latitude,
+            longitude: placesInfo.longitude,
+            description: placesInfo.description
         });
-        if(image)
+        if (image)
             place.image = image.image.tempFilePath;
         if (!dbConnection.createObject(place, Place)) {
             res.status(200).send(place);
         } else {
-            res.status(400).send({"error":"Failed to save, check data type"});
+            res.status(400).send({ "error": "Failed to save, check data type" });
         }
     }
 }
 
 function updatePlaceWithId(apiVersion, req, res, next) {
-    Place.findByIdAndUpdate(req.params.id, req.body, 
-        function(err, response){
+    Place.findByIdAndUpdate(req.params.id, req.body,
+        function (err, response) {
             if (!err)
                 res.status(200).send(response);
             else
-                res.status(400).send({"error":"Failed to save, check data type" + err});
-    });
+                res.status(400).send({ "error": "Failed to save, check data type" + err });
+        });
 }
+
+
+function moveFile(file) {
+    return new Promise((res, rej) => {
+        file.image.mv('/Users/deep/Development/UploadedImages' + file.image.tempFilePath + '.jpg', function (err) {
+            if (err)
+                rej(err);
+            else
+                res();
+        });
+    })
+}
+
+
 module.exports = {
     getAllPlaces,
     getPlaceWithId,
